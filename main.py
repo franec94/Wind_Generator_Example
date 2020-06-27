2c67cbdfa1e9fd5c1c38f011f4b2286f7d3a5bd3
francesco chiarlo <franec94@gmail.com>
2020-06-27 12:48:43 +0000

    Wind generator notebook ipynb added

--- main.py
+++ main.py
@@ -26,6 +26,15 @@ def main():
 
   It = compute_It(U_nom, R_stat, R_rot, R_app, s, X_Dsr)
   print("%.2f %s" % (It, res_uim_dict['It']))
+
+  # np.linspace(start, stop, num=N, endpoint=True, retstep=False, dtype=None, axis=0)
+  rows, cols = N, 6
+  X = create_data_matrix(rows=rows, cols=cols, U_nom=U_nom, R_stat=R_stat, R_rot=R_rot, R_app=R_app, s=s, X_Dsr=X_Dsr)
+
+  cols_X = "U_nom,R_stat,R_rot,R_app,s,X_Dsr".split(',')
+  df_X = pd.DataFrame(data=X, columns=cols_X)
+
+  print(df_X.head())
   pass
 
 if __name__ == "__main__":
